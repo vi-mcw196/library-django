176c7ace4bb60788a595001a9ef8777510f2a280
vi-mcw196 <moldlova50@gmail.com>
2022-04-05 19:55:11 +0200

    update catalog/urls.py

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
