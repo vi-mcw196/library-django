52591a7dfb5660220bc4fcd0f91d8cd801214229
vi-mcw196 <moldlova50@gmail.com>
2022-05-02 21:11:36 +0200

    added num_visits into views.py

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -5,25 +5,24 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance
 
 
-def index(request: HttpRequest) -> HttpResponse:
+def index(request):
     """View function for home page of site."""
-
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
-    # Available books (status = 'a')
+    # Available copies of books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
-    # The 'all()' is implied by default.
-    num_authors = Author.objects.count()
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits+1
 
-    context = {
-        'num_books': num_books,
-        'num_instances': num_instances,
-        'num_instances_available': num_instances_available,
-        'num_authors': num_authors,
-    }
-
-    # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
+    )
