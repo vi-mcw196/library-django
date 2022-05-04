46958c69c8b42b4d4f5ba90a78299012e4040926
vi-mcw196 <moldlova50@gmail.com>
2022-05-04 02:49:23 +0200

    added ModalForm

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
