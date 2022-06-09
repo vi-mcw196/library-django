ae1f73e13f3fea4e27a90bca5aa08dc3905e98c2
vi-mcw196 <moldlova50@gmail.com>
2022-06-09 10:50:04 +0200

    add list-borrowed-url

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 
     path('authors/', views.AuthorListView.as_view(), name='authors'),
