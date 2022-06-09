09c0642d2fe8dc523cdbca72b6c17ea375d0ba2d
vi-mcw196 <moldlova50@gmail.com>
2022-06-09 02:44:39 +0200

    upd urls

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -9,7 +9,9 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
     path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 
